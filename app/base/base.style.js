487c853b67f49211c26e4add9748f1dd76893135
Eduardo Juan José Alvarez <edujjalvarez@gmail.com>
2020-07-02 16:30:05 -0300

    Fix error

--- app/base/base.style.js
+++ app/base/base.style.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Dimensions } from "react-native";
 
-export const Dimensions = {
+export const dimensions = {
   fullHeight: Dimensions.get("window").height,
   fullWidth: Dimensions.get("window").width,
 };
