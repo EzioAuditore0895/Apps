41b0f9adaf1243c9257c5f14cd921696ccb765d2
Eduardo Juan José Alvarez <edujjalvarez@gmail.com>
2020-07-03 19:04:33 -0300

    Toast no funciona.

--- app/ui/intro/login/Login.tsx
+++ app/ui/intro/login/Login.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View, Text } from "react-native";
 import styles from "./login.style";
 import { Button } from "react-native-elements";
 import { useNavigation, StackActions } from "@react-navigation/native";
+import Toast from "react-native-easy-toast";
 
 export default function Login() {
+  const toastRef = useRef();
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
@@ -12,9 +14,11 @@ export default function Login() {
       <Button
         title="Ir a la página principal"
         onPress={() => {
-          navigation.dispatch(StackActions.replace("MainBottomTabNavigator"));
+          // navigation.dispatch(StackActions.replace("MainBottomTabNavigator"));
+          toastRef.current.show("Mensaje.");
         }}
       />
+      <Toast ref={toastRef} position="center" opacity={0.9} />
     </View>
   );
 }
