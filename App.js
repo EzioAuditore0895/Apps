e9243b18ba063f79d0b0b044f2f38f5d20364869
Eduardo Juan José Alvarez <edujjalvarez@gmail.com>
2020-07-03 18:21:04 -0300

    Borro todo el proyecto para migrar a la versión typescript.

--- App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import firebaseApp from "./app/services/firebase/firebase.service";
-import UiStackNavigator from "./app/ui/UiStackNavigator";
-
-export default function App() {
-  return <UiStackNavigator />;
-}
