a374b8100703102adae85b8541265e544c736a01
Eduardo Juan José Alvarez <edujjalvarez@gmail.com>
2020-07-06 17:43:23 -0300

    Se modifico servicio base.

--- app/services/base.service.ts
+++ app/services/base.service.ts
@@ -2,7 +2,6 @@ import { Base } from "../models/base.model";
 import { firebaseApp } from "../services/firebase/firebase.service";
 import firebase from "firebase";
 import "firebase/firestore";
-import { BaseCallback } from "./base.callback";
 
 const db = firebase.firestore(firebaseApp);
 
@@ -14,111 +13,99 @@ export class BaseService<T extends Base> {
     this.reference = reference;
   }
 
-  getAll(callback: BaseCallback<T>): void {
-    db.collection(`${this.reference}`)
-      .get()
-      .then((querySnapshot) => {
-        const entities: T[] = [];
-        querySnapshot.forEach(function (doc) {
-          // doc.data() is never undefined for query doc snapshots
-          // console.log(doc.id, " => ", doc.data());
-          entities.push(<T>doc.data());
+  getAll(): Promise<T[]> {
+    return new Promise((resolve, reject) => {
+      db.collection(`${this.reference}`)
+        .get()
+        .then((querySnapshot) => {
+          const entities: T[] = [];
+          querySnapshot.forEach(function (doc) {
+            // doc.data() is never undefined for query doc snapshots
+            // console.log(doc.id, " => ", doc.data());
+            entities.push(<T>doc.data());
+          });
+          resolve(entities);
+        })
+        .catch(() => {
+          reject();
         });
-        if (callback) {
-          callback.onResults(entities);
-        }
-      })
-      .catch(() => {
-        if (callback) {
-          callback.onError();
-        }
-      });
+    });
   }
 
-  public getById(id: string, callback: BaseCallback<T>): void {
-    db.collection(`${this.reference}`)
-      .doc(`${id}`)
-      .get()
-      .then((doc) => {
-        const entity: T | null = doc.exists ? <T>doc.data() : null;
-        if (callback) {
-          callback.onResult(entity);
-        }
-      })
-      .catch(() => {
-        if (callback) {
-          callback.onError();
-        }
-      });
+  public getById(id: string): Promise<T | null> {
+    return new Promise((resolve, reject) => {
+      db.collection(`${this.reference}`)
+        .doc(`${id}`)
+        .get()
+        .then((doc) => {
+          const entity: T | null = doc.exists ? <T>doc.data() : null;
+          resolve(entity);
+        })
+        .catch(() => {
+          reject();
+        });
+    });
   }
 
-  public add(entity: T, callback: BaseCallback<T>): void {
+  public add(entity: T): Promise<boolean> {
     Base.setInitialValues(entity);
     console.log(`${this.TAG} > add > model`, entity);
-    db.collection(`${this.reference}/${entity.id}`)
-      .add(entity)
-      .then(() => {
-        if (callback) {
-          callback.onSuccess();
-        }
-      })
-      .catch(() => {
-        if (callback) {
-          callback.onError();
-        }
-      });
+    return new Promise((resolve, reject) => {
+      db.collection(`${this.reference}/${entity.id}`)
+        .add(entity)
+        .then(() => {
+          resolve(true);
+        })
+        .catch(() => {
+          reject();
+        });
+    });
   }
 
-  public edit(entity: T, callback: BaseCallback<T>): void {
+  public edit(entity: T): Promise<boolean> {
     Base.setValuesOnEdit(entity);
     console.log(`${this.TAG} > edit > model`, entity);
-    db.collection(`${this.reference}`)
-      .doc(`${entity.id}`)
-      .update(entity)
-      .then(() => {
-        if (callback) {
-          callback.onSuccess();
-        }
-      })
-      .catch(() => {
-        if (callback) {
-          callback.onError();
-        }
-      });
+    return new Promise((resolve, reject) => {
+      db.collection(`${this.reference}`)
+        .doc(`${entity.id}`)
+        .update(entity)
+        .then(() => {
+          resolve(true);
+        })
+        .catch(() => {
+          reject();
+        });
+    });
   }
 
-  public softDelete(entity: T, callback: BaseCallback<T>): void {
+  public softDelete(entity: T): Promise<boolean> {
     Base.setValuesOnDelete(entity);
     console.log(`${this.TAG} > add > softDelete`, entity);
-    db.collection(`${this.reference}`)
-      .doc(`${entity.id}`)
-      .update(entity)
-      .then(() => {
-        if (callback) {
-          callback.onSuccess();
-        }
-      })
-      .catch(() => {
-        if (callback) {
-          callback.onError();
-        }
-      });
+    return new Promise((resolve, reject) => {
+      db.collection(`${this.reference}`)
+        .doc(`${entity.id}`)
+        .update(entity)
+        .then(() => {
+          resolve(true);
+        })
+        .catch(() => {
+          reject();
+        });
+    });
   }
 
-  public hardDelete(entity: T, callback: BaseCallback<T>): void {
+  public hardDelete(entity: T): Promise<boolean> {
     console.log(`${this.TAG} > add > hardDelete`, entity);
-    db.collection(`${this.reference}`)
-      .doc(`${entity.id}`)
-      .delete()
-      .then(() => {
-        if (callback) {
-          callback.onSuccess();
-        }
-      })
-      .catch(() => {
-        if (callback) {
-          callback.onError();
-        }
-      });
+    return new Promise((resolve, reject) => {
+      db.collection(`${this.reference}`)
+        .doc(`${entity.id}`)
+        .delete()
+        .then(() => {
+          resolve(true);
+        })
+        .catch(() => {
+          reject();
+        });
+    });
   }
 }
