6b3362db20da81c09e982ff7177a0b9547410968
dmarquillas <dmarquillas@gmail.com>
2020-07-07 17:38:06 -0300

    Ignore warning

--- App.tsx
+++ App.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import { YellowBox } from "react-native";
 import { firebaseApp } from "./app/services/firebase/firebase.service";
 import UiStackNavigator from "./app/ui/UiStackNavigator";
-import { YellowBox } from "react-native";
 
 YellowBox.ignoreWarnings([
+  "Setting a timer",
   "Animated: `useNativeDriver` was not specified. This is a required option and must be explicitly set to `true` or `false`",
 ]);
-
-YellowBox.ignoreWarnings(["Setting a timer"]);
 
 export default function App() {
   const firebase = firebaseApp;
